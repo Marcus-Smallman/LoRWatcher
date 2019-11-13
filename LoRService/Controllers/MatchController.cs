8ba715b24f0405d4d8c6d978724e8ff5a5fac010
marcus-smallman <marcussmallman@live.co.uk>
2019-11-13 21:24:08 +0000

    Added controller for match reporting

---
+++ LoRService/Controllers/MatchController.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace LoRService.Controllers
+{
+    [Route("api/v1/match")]
+    [ApiController]
+    public class MatchController : ControllerBase
+    {
+        [HttpPost("report")]
+        public IActionResult Report(string value)
+        {
+            return new ContentResult
+            {
+                Content = JsonConvert.SerializeObject(value),
+                ContentType = "application/json",
+                StatusCode = (int)HttpStatusCode.Created
+            };
+        }
+    }
+}
