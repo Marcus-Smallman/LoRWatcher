c9b093cb9b4ec25d1a8404fc7db747370938b2c8
Marcus Smallman <marcussmallman@live.co.uk>
2020-05-27 22:06:05 +0100

    Added fix for loading chart

--- LoRWatcher/Utils/Retry.cs
+++ LoRWatcher/Utils/Retry.cs
@@ -12,7 +12,7 @@ namespace LoRWatcher.Utils
         /// <summary>
         /// Retries execution of an async lamda function.
         /// </summary>
-        /// <param name="func">A function that should return a non null object if no more retires need to occur; else null to carry on retrying.</param>
+        /// <param name="func">An async function that should return a non null object if no more retires need to occur; else null to carry on retrying.</param>
         /// <returns>A task.</returns>
         public static async Task<T> InvokeAsync<T>(Func<Task<T>> func)
             where T : class
@@ -37,7 +37,7 @@ namespace LoRWatcher.Utils
         /// <summary>
         /// Retries execution of an async lamda function.
         /// </summary>
-        /// <param name="func">A function that should return true if no more retires need to occur; else false to carry on retrying.</param>
+        /// <param name="func">An async function that should return true if no more retires need to occur; else false to carry on retrying.</param>
         /// <returns>A task.</returns>
         public static async Task<bool> InvokeAsync(Func<Task<bool>> func)
         {
@@ -51,6 +51,35 @@ namespace LoRWatcher.Utils
                 }
 
                 await Task.Delay(WaitTimeMS);
+
+                retryCount++;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Retries execution of an lamda function.
+        /// </summary>
+        /// <param name="func">A function that should return true if no more retires need to occur; else false to carry on retrying.</param>
+        /// <param name="waitTimeMs">An optional parameter to set the wait time between retries. Defaults to 1000 if null.</param>
+        /// <param name="numOfRetries">An optional parameter to set the number of retries. Defaults to 5 if null.</param>
+        /// <returns>A task.</returns>
+        public static async Task<bool> InvokeAsync(Func<bool> func, int? waitTimeMs = null, int? numOfRetries = null)
+        {
+            numOfRetries = numOfRetries.HasValue ? numOfRetries.Value : NumOfRetries;
+            waitTimeMs = waitTimeMs.HasValue ? waitTimeMs.Value : WaitTimeMS;
+
+            var retryCount = 0;
+            while (retryCount < numOfRetries.Value)
+            {
+                var result = func();
+                if (result == true)
+                {
+                    return result;
+                }
+
+                await Task.Delay(waitTimeMs.Value);
 
                 retryCount++;
             }
