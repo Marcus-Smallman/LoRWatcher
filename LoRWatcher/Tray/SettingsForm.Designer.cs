1299b838b4a64cedccd4e883695ba73d4ee76774
marcus-smallman <marcussmallman@live.co.uk>
2020-12-27 01:26:48 +0000

    Minor settings dialog changes

--- LoRWatcher/Tray/SettingsForm.Designer.cs
+++ LoRWatcher/Tray/SettingsForm.Designer.cs
@@ -52,70 +52,78 @@ namespace LoRWatcher.Tray
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(6, 4);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(156, 32);
+            this.label1.Size = new System.Drawing.Size(64, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "LoR Game";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(200, 45);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(103, 32);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Address:";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(309, 42);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(200, 39);
-            this.textBox1.TabIndex = 4;
-            this.textBox1.Text = configuration["LoR:Address"];
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(242, 100);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(61, 32);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Port:";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(309, 97);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(200, 39);
-            this.textBox2.TabIndex = 5;
-            this.textBox2.Text = configuration["LoR:Port"];
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(12, 139);
+            this.label2.Location = new System.Drawing.Point(6, 65);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(207, 32);
+            this.label2.Size = new System.Drawing.Size(90, 15);
             this.label2.TabIndex = 1;
             this.label2.Text = "Watcher Client";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(110, 23);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Address:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(130, 48);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Port:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(166, 20);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(110, 23);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = configuration["LoR:Address"];
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(166, 45);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(110, 23);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = configuration["LoR:Port"];
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(242, 177);
+            this.label5.Location = new System.Drawing.Point(130, 85);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(61, 32);
+            this.label5.Size = new System.Drawing.Size(32, 15);
             this.label5.TabIndex = 6;
             this.label5.Text = "Port:";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(309, 174);
+            this.textBox3.Location = new System.Drawing.Point(166, 82);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(200, 39);
+            this.textBox3.Size = new System.Drawing.Size(110, 23);
             this.textBox3.TabIndex = 7;
             this.textBox3.Text = configuration["Client:Port"];
             // 
@@ -123,70 +131,78 @@ namespace LoRWatcher.Tray
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label6.Location = new System.Drawing.Point(12, 209);
+            this.label6.Location = new System.Drawing.Point(6, 98);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(108, 32);
+            this.label6.Size = new System.Drawing.Size(51, 15);
             this.label6.TabIndex = 8;
             this.label6.Text = "Logging";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(151, 249);
+            this.label7.Location = new System.Drawing.Point(88, 117);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(152, 32);
+            this.label7.Size = new System.Drawing.Size(74, 15);
             this.label7.TabIndex = 9;
             this.label7.Text = "Write To File:";
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(309, 252);
+            this.checkBox1.Location = new System.Drawing.Point(166, 118);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(28, 27);
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
             this.checkBox1.TabIndex = 10;
             this.checkBox1.UseVisualStyleBackColor = true;
             this.checkBox1.Checked = bool.Parse(configuration["LoggerSettings:WriteToFile"]);
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(309, 296);
+            this.textBox4.Location = new System.Drawing.Point(166, 139);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(200, 39);
+            this.textBox4.Size = new System.Drawing.Size(110, 23);
             this.textBox4.TabIndex = 11;
             this.textBox4.Text = configuration["LoggerSettings:FileDirectory"];
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(166, 165);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(110, 23);
+            this.textBox5.TabIndex = 12;
+            this.textBox5.Text = configuration["LoggerSettings:CleanupPeriodMinutes"];
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(143, 299);
+            this.label8.Location = new System.Drawing.Point(83, 142);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(160, 32);
+            this.label8.Size = new System.Drawing.Size(79, 15);
             this.label8.TabIndex = 13;
             this.label8.Text = "File Directory:";
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(309, 351);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(200, 39);
-            this.textBox5.TabIndex = 12;
-            this.textBox5.Text = configuration["LoggerSettings:CleanupPeriodMinutes"];
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(28, 354);
+            this.label9.Location = new System.Drawing.Point(25, 168);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(275, 32);
+            this.label9.Size = new System.Drawing.Size(137, 15);
             this.label9.TabIndex = 14;
             this.label9.Text = "Cleanup Period Minutes:";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(272, 422);
+            this.button1.Location = new System.Drawing.Point(146, 198);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(137, 46);
+            this.button1.Size = new System.Drawing.Size(74, 22);
             this.button1.TabIndex = 15;
             this.button1.Text = "Save";
             this.button1.UseVisualStyleBackColor = true;
@@ -194,9 +210,10 @@ namespace LoRWatcher.Tray
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(129, 422);
+            this.button2.Location = new System.Drawing.Point(69, 198);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(137, 46);
+            this.button2.Size = new System.Drawing.Size(74, 22);
             this.button2.TabIndex = 16;
             this.button2.Text = "Cancel";
             this.button2.UseVisualStyleBackColor = true;
@@ -204,9 +221,9 @@ namespace LoRWatcher.Tray
             // 
             // SettingsForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(522, 480);
+            this.ClientSize = new System.Drawing.Size(281, 225);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label9);
@@ -224,11 +241,12 @@ namespace LoRWatcher.Tray
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.MaximizeBox = false;
             this.Name = "SettingsForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Settings";
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.MaximizeBox = false;
             this.ResumeLayout(false);
             this.PerformLayout();
 
