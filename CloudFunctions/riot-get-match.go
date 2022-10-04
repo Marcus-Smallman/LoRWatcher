20d13920daad7228de0f899a78ed0b5a7b68df3f
Marcus-Smallman <marcussmallman@live.co.uk>
2022-10-04 22:47:44 +0100

    Added get match cloud function

---
+++ CloudFunctions/riot-get-match.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func Main(args map[string]interface{}) map[string]interface{} {
+	msg := make(map[string]interface{})
+	matchId, ok := args["matchId"].(string)
+	if !ok {
+		msg["body"] = `{"status":{"message":"matchId is required","status_code":400}}`
+		return msg
+	}
+
+	region, ok := args["region"].(string)
+	if !ok {
+		region = "europe"
+	}
+
+	client := &http.Client{}
+	url := fmt.Sprintf("https://%s.api.riotgames.com/lor/match/v1/matches/%s", region, matchId)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Println(err)
+		return msg
+	}
+
+	apikey := os.Getenv("riotapikey")
+	req.Header.Set("X-Riot-Token", apikey)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return msg
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return msg
+	}
+
+	msg["body"] = string(body)
+
+	return msg
+}
